8400873c727df2ce2bed199734614b232aafdfc7
ZuolongHe <hezuolong161460@163.com>
2025-03-04 10:35:37 +0800

    拦截器优化

---
+++ src/main/java/com/hmdp/utils/RefreshCheckInterceptor.java
@@ -0,0 +1,34 @@
+package com.hmdp.utils;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @Title hmdp
+ * @Author hzl
+ * @Date 2025/3/4 10:21
+ * @Description 拦截优化
+ */
+
+@Component
+public class RefreshCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
