9e8e23479416d2ac3947865881c897b759867b78
ZuolongHe <hezuolong161460@163.com>
2025-03-08 20:20:43 +0800

    缓存击穿

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,13 @@
+package com.hmdp.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Title hmdp
+ * @Author hzl
+ * @Date 2025/3/6 22:04
+ * @Description
+ */
+@Component
+public class RedisIdWorker {
+}
