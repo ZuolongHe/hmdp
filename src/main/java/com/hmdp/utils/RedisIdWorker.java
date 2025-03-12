18aeb9b27a121ee93a6d9eb20c604f920b4a9764
ZuolongHe <hezuolong161460@163.com>
2025-03-12 09:02:01 +0800

    全局唯一id

--- src/main/java/com/hmdp/utils/RedisIdWorker.java
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -1,13 +1,53 @@
 package com.hmdp.utils;
 
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @Title hmdp
  * @Author hzl
  * @Date 2025/3/6 22:04
- * @Description
+ * @Description 生成全局唯一id
+ *
  */
 @Component
 public class RedisIdWorker {
+
+    // 起始时间戳
+    private static final long START_TIME = 1577836800L;
+
+    private static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    // 生成全局唯一id
+    public long onlyId(String keyValue){
+        // 1.生成时间戳
+        long distanceTime = LocalDateTime.now().toEpochSecond(ZoneOffset.UTC) - START_TIME;
+        // 2.生成序列号
+        String yyyyMMdd = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+        Long increment = stringRedisTemplate
+                .opsForValue().increment("inc:" + keyValue + ":" + yyyyMMdd);
+        // 3.拼接时间戳+序列号
+        return distanceTime << COUNT_BITS | increment;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime of = LocalDateTime
+                .of(2020, 1, 1, 0, 0, 0);
+        long second = of.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(second);
+    }
+
+
+
+
+
+
 }
--- src/test/test/com/hmdp/HmdpTest.java
+++ src/test/test/com/hmdp/HmdpTest.java
@@ -2,9 +2,13 @@ package com.hmdp;
 
 import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @Title hmdp
@@ -21,9 +25,30 @@ public class HmdpTest {
     @Autowired
     private IShopService iShopService;
 
+    @Autowired
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     @Test
     void testRedisData(){
         shopService.shopToRedis(1L, 10L);
+    }
+
+    @Test
+    void testOnlyId(){
+        Runnable task = () -> {
+            for (int i = 1; i <= 100 ; i++) {
+                long order = redisIdWorker.onlyId("order");
+                System.out.println("id:" + i + ":" + order);
+            }
+        };
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(task);
+        }
+
+        executorService.shutdown();
+
     }
 
 }
